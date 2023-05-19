90dca94a10fbc11a9ad03435476783a3d170b5ce
karishmas6 <carishmashukla28@gmail.com>
2023-05-19 20:10:16 +0530

    fix: remove assignment

--- tools.go
+++ tools.go
@@ -90,7 +90,7 @@ func (t *Tools) UploadFiles(req *http.Request, uploadDir string, rename ...bool)
 					return nil, errors.New("The uploaded file type is not allowed")
 				}
 
-				_, err := infile.Seek(0, 0)
+				_, err = infile.Seek(0, 0)
 				if err != nil {
 					return nil, err
 				}
