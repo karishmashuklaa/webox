fd487ae71183d6773083bec533fb755f93ae9ddc
karishmas6 <carishmashukla28@gmail.com>
2023-05-19 21:20:41 +0530

    feat: error check

--- tools.go
+++ tools.go
@@ -144,6 +144,9 @@ func (t *Tools) CreateDirIfNotExist(path string) error {
 	const mode = 0755
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err := os.MkdirAll(path, mode)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
